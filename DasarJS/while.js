c082400df77536949ea73a627ea78e42a2927797
Tegar Kusuma <tegarkusuma063@gmail.com>
2025-09-18 21:00:59 +0700

    While dan Latihan

---
+++ DasarJS/while.js
@@ -0,0 +1,11 @@
+let nilai = 1;
+
+// dihentikan ketika nilai sudah lebih dari 10
+while (nilai <= 10) {
+    console.log(`Nilai saat ini: ${nilai}x`);
+    nilai++;
+}
+
+// while (true) {
+//     console.log('Ini akan terus berulang selamanya');
+// }
